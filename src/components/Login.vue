14bcb0fbc0b3d12250ac1931b1f93779360b5e2f
jackzhang199598 <2758354479@qq.com>
2018-12-20 23:19:19 +0800

    登入完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,114 @@
 <template>
   <div class="login">
-    登入
+    <el-form ref="form" :model="form" label-width="80px" :rules="rules" status-icon>
+      <img src="@/assets/avatar.jpg" alt srcset>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户名为空', trigger: 'blur' },
+          {
+            min: 3,
+            max: 9,
+            message: '用户名长度在 3 到 9个字符',
+            trigger: 'change'
+          }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          {
+            min: 3,
+            max: 9,
+            message: '密码长度在 3 到 9个字符',
+            trigger: 'change'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset() {
+      this.$refs.form.resetFields()
+    },
+    login() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          axios({
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              // this.$message.success('登录成功')
+              this.$message({
+                message: '登录成功',
+                type: 'success',
+                duration: 1000
+              })
+              localStorage.setItem('token111', res.data.data.token)
+              this.$router.push('/home')
+            } else {
+              this.$message({
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 1000
+              })
+            }
+          })
+        } else {
+          return false
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less'>
+.login {
+  height: 100%;
+  width: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .el-form {
+    width: 400px;
+    background-color: #fff;
+    margin: 200px auto;
+    padding: 75px 40px 15px;
+    position: relative;
+    img {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -70px;
+      border-radius: 50%;
+      border: 5px solid #fff;
+    }
+    .el-button:nth-child(2) {
+      margin-left: 80px;
+    }
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from 'components/Login'
-
+import Home from '@/components/Home'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
+      redirect: './login'
+    },
+    {
+      path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    next()
+    return
+  }
+  let token = localStorage.getItem('token111')
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+export default router
